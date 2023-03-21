35b5c821e1cf4c50d59840a6c2ad8b2e6a034d14
Pavel Paulavets <paulavetspavel@outlook.com>
2023-03-21 20:10:55 +0300

    Refactoring JS & HTML

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 import mistakes from './data.js';
 
-const mainItems = document.querySelector('.main__items');
-for (const mistake of mistakes) {
-  mainItems.innerHTML += `
-<div class="main__item"><p class="main-item__number">${mistake.id}</p> <div class="main-item__title"> ${mistake.title}</div><div class="main-item__body">${mistake.content}</div><div class="main-item__link">
-<a href="#">Learm more</a>
-</div>
-</div>`;
+const mainItems = document.querySelector('.mistakes__items');
+
+const displayMistakes = (mistakes) => {
+  mistakes.forEach(mistake => {
+    const mistakeItem = `<div class="mistakes__item">
+    <p class="mistakes-item__number">${mistake.id}</p> 
+    <div class="mistakes-item__title"> ${mistake.title}</div>
+    <div class="mistakes-item__body">${mistake.content}</div>
+    <div class="mistakes-item__link"><a href="#">Learm more</a></div>
+                         </div>`
+    mainItems.insertAdjacentHTML('beforeend', mistakeItem)
+  })
 }
+
+displayMistakes(mistakes)
