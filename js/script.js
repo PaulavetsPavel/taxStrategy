d0dff89bad1f76852cfa9fe9cafdaf2bc5535ad9
Pavel Paulavets <paulavetspavel@outlook.com>
2023-03-23 20:28:43 +0300

    Add new section

--- js/script.js
+++ js/script.js
@@ -12,11 +12,12 @@ const displayMistakes = (mistakes) => {
     <p class="mistakes-item__number">${mistake.id}</p> 
     <div class="mistakes-item__title"> ${mistake.title}</div>
     <div class="mistakes-item__body">${mistake.content}</div>
-    <div class="mistakes-item__link"><a href="#">Learm more</a></div>
+    <div class="learn-more__link"><a href="#">Learm more</a></div>
                          </div>`
     mistakesItems.insertAdjacentHTML('beforeend', mistakeItem)
   })
 }
+
 const displaySocials = (socials) => {
   socialItems.innerHTML = ''
   socials.forEach(({ socialName }) => {
